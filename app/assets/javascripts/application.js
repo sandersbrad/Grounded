fe9c8da2c078451df6732d3dc9eb2a7b2adc65fa
Brad Sanders <sandersbrad@gmail.com>
2015-08-11 15:36:27 -0700

    Fix migration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,5 +20,4 @@
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  property_id :integer          not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Follow < ActiveRecord::Base
 
   validates_uniqueness_of :user_id, scope: :property_id
