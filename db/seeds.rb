a8bae73c48c8fc700656b838527d4722c286dd37
Brad Sanders <sandersbrad@gmail.com>
2015-08-17 14:45:54 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-
-users = User.create([{ email: 'sandersbrad@gmail.com', password: 'dummypass' },
-                     { email: 'fayekeegan@gmail.com', password: 'dumbass' }
-                    ])
-
-properties = Property.create([{ street_number: '26', street: 'Lexington St',
-                                city: 'San Francisco', state: 'CA', zip: '94110',
-                                num_beds: 4, num_baths: 1, price: 1000000,
-                                property_type: 'Apartment' },
-
-                              { street_number: '2822', street: 'Octavia St',
-                                city: 'San Francisco', state: 'CA', zip: '94114',
-                                num_beds: 4, num_baths: 1, price: 1200000,
-                                property_type: 'Apartment'  },
-
-                              { street_number: '99', street: 'Walter St',
-                                city: 'San Francisco', state: 'CA', zip: '94118',
-                                num_beds: 3, num_baths: 1, price: 1400000,
-                                property_type: 'Apartment'  }
-                              ])
-
-
-follows = Follow.create([{ user_id: User.first.id, property_id: Property.first.id },
-                         { user_id: User.first.id, property_id: Property.second.id },
-                         { user_id: User.last.id, property_id: Property.last.id }
-                        ])
-images = Image.create!([
+Follow.create!([
+  {user_id: 2, property_id: 3}
+])
+Image.create!([
   {property_id: 1, image_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/v1439764856/cqgsbbc6dv034hp2ujzw.jpg", thumb_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/c_limit,h_60,w_90/v1439764856/cqgsbbc6dv034hp2ujzw.jpg"},
   {property_id: 1, image_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/v1439764865/zpcotmnlf8pl5hizuwow.jpg", thumb_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/c_limit,h_60,w_90/v1439764865/zpcotmnlf8pl5hizuwow.jpg"},
   {property_id: 2, image_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/v1439764894/kjv0t7viktlblijdasjp.jpg", thumb_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/c_limit,h_60,w_90/v1439764894/kjv0t7viktlblijdasjp.jpg"},
   {property_id: 2, image_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/v1439764899/nxjkvkk49dsz3e0b2nld.jpg", thumb_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/c_limit,h_60,w_90/v1439764899/nxjkvkk49dsz3e0b2nld.jpg"},
   {property_id: 3, image_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/v1439764915/e2s2se2vmkn4acablwgm.jpg", thumb_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/c_limit,h_60,w_90/v1439764915/e2s2se2vmkn4acablwgm.jpg"},
   {property_id: 3, image_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/v1439764921/tham0qs7zrrrkckdun5m.jpg", thumb_url: "http://res.cloudinary.com/dwhvrflyu/image/upload/c_limit,h_60,w_90/v1439764921/tham0qs7zrrrkckdun5m.jpg"}
+])
+Property.create!([
+  {street_number: "26", unit: nil, street: "Lexington St", city: "San Francisco", state: "CA", zip: "94110", description: nil, num_beds: 4, num_baths: 1, price: 1000000, property_type: "Apartment"},
+  {street_number: "2822", unit: nil, street: "Octavia St", city: "San Francisco", state: "CA", zip: "94114", description: nil, num_beds: 4, num_baths: 1, price: 1200000, property_type: "Apartment"},
+  {street_number: "99", unit: nil, street: "Walter St", city: "San Francisco", state: "CA", zip: "94118", description: nil, num_beds: 3, num_baths: 1, price: 1400000, property_type: "Apartment"}
+])
+User.create!([
+  {email: "fayekeegan@gmail.com", password_digest: "$2a$10$YnfY7YucIZ9xjAV0YiAjzeOc13xc4t2rQ8f7d.g/TaU2rLFdrglZ6", session_token: "wtFfqiQGwULU_zSOIl9lPg", phone_number: nil, admin: nil},
+  {email: "sandersbrad@gmail.com", password_digest: "$2a$10$T8sQpHKcjmKcnO5IvZUTY.k4rB/7nauq2q8mrJmfUPabCKwBzUMQi", session_token: "6p94ImONfgptnTkafUwGDw", phone_number: nil, admin: nil}
 ])
